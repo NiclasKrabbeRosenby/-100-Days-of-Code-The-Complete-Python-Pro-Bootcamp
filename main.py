5e1437b2c27bf4e0878a1c76542705c4c3118c31
krabbe89 <33464514-krabbe89@users.noreply.replit.com>
2024-04-17 07:38:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print("Hello World!\nHello world! second line")
+print("Hello " + "Angela")
+print("Hello" + " " + "Angela")
+
+input("Hello " + input("What is your name?"))
